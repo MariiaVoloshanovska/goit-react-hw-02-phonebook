b1188a646f59a95922de44482148c6c35fe08e23
MariiaVoloshanovska <masterskaiamary@gmail.com>
2023-06-24 20:19:35 +0300

    1

--- src/components/ContactFilter/contactFilter.styled.jsx
+++ src/components/ContactFilter/contactFilter.styled.jsx
@@ -1,14 +1,24 @@
 import styled from 'styled-components';
 
 const FilterDelBtn = styled.button`
-  cursor: pointer;
-  margin-left: 20px;
   border-radius: 5px;
-  background-color: #fff;
-  transition: background-color 500ms cubic-bezier(0.165, 0.84, 0.44, 1);
-
+  transition: background-color 300ms cubic-bezier(0.165, 0.84, 0.44, 1);
+  font-size: 15px;
+    font-weight: 500;
+    color: rgba(51, 51, 51, 0.8);
+    margin-top: 30px;
+    padding: 4px;
+    border: none;
+    box-shadow: 0 0 2px #333;
+    cursor: pointer;
+    background-color: rgba(178, 146, 171, 0.742);
+    border-radius: 5px;
+    transition-duration: 400ms;
+    margin-left: 20px;
   &:hover {
-    background-color: aqua;
+    cursor: pointer;
+    background-color: #BC8F8F;
+    
   }
 `;
 
--- src/components/app.styled.jsx
+++ src/components/app.styled.jsx
@@ -3,6 +3,13 @@ import styled from 'styled-components';
 const PhoneBookStyle = styled.div`
   width: 600px;
   margin: 0 auto;
+  margin: 50px auto;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: rgba(178 146 171 / 50%);
+    box-shadow: 0px 0px 8px 0px #33333347;
 `;
 
 export { PhoneBookStyle };
